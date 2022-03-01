196efe42ecc06d4962d0bfb5c2a1780f048d22df
Konrad Goławski <konrad@golawski.it>
2022-03-01 22:14:04 +0100

    Add missing docsrs cfgs

--- chksum/src/lib.rs
+++ chksum/src/lib.rs
@@ -1,12 +1,16 @@
+#![cfg_attr(docsrs, feature(doc_cfg))]
+
 #[rustfmt::skip]
 pub use chksum_arch as arch;
 #[cfg(feature = "async")]
+#[cfg_attr(docsrs, doc(cfg(feature = "async")))]
 pub use chksum_async as r#async;
 #[rustfmt::skip]
 pub use chksum_config as config;
 #[rustfmt::skip]
 pub use chksum_hash as hash;
 #[cfg(feature = "sync")]
+#[cfg_attr(docsrs, doc(cfg(feature = "sync")))]
 pub use chksum_sync as sync;
 pub use chksum_traits::*;
 

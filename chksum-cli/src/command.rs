b418b9e8d3e711b4acdc6d9565bb7b8f9e99859e
Konrad Goławski <konrad@golawski.it>
2022-03-01 18:52:32 +0100

    Add information about implemented hash algorithms

--- chksum-cli/src/command.rs
+++ chksum-cli/src/command.rs
@@ -3,14 +3,17 @@ use clap::{arg, command, Command};
 use super::parse;
 
 pub fn create() -> Command<'static> {
-    command!("chksum").author("").args([
-        arg!(paths: <PATH> "Path to file or directory").multiple_values(true),
-        arg!(-H --hash <HASH> "Choose hashing algorithm")
-            .required(false)
-            .validator(|value| parse::hash(value).map(|_| ())),
-        arg!(-P --"with-paths" "Use paths to calculate digest"),
-        arg!(-s --"chunk-size" <SIZE> "Set chunk size of processing data")
-            .required(false)
-            .validator(|value| parse::chunk_size(value).map(|_| ())),
-    ])
+    command!("chksum")
+        .author("")
+        .args([
+            arg!(paths: <PATH> "Path to file or directory").multiple_values(true),
+            arg!(-H --hash <HASH> "Choose hashing algorithm")
+                .required(false)
+                .validator(|value| parse::hash(value).map(|_| ())),
+            arg!(-P --"with-paths" "Use paths to calculate digest"),
+            arg!(-s --"chunk-size" <SIZE> "Set chunk size of processing data")
+                .required(false)
+                .validator(|value| parse::chunk_size(value).map(|_| ())),
+        ])
+        .after_help("Implemented hash algorithms:\n - MD5,\n - SHA-1.")
 }

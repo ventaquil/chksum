5a4950278474095d66e3401161763c03c00bed53
Konrad Goławski <konrad@golawski.it>
2022-08-05 13:47:49 +0200

    Style fix for CLI application tests

--- chksum-cli/tests/cmd_tests.rs
+++ chksum-cli/tests/cmd_tests.rs
@@ -1,9 +1,8 @@
-use std::io;
-use std::result;
+use std::{io, result};
 
-use assert_cmd::Command;
 use assert_cmd::cargo::CargoError;
-use assert_fs::fixture::{TempDir, FixtureError};
+use assert_cmd::Command;
+use assert_fs::fixture::{FixtureError, TempDir};
 use assert_fs::prelude::*;
 use thiserror::Error;
 
@@ -80,16 +79,56 @@ fn test_chunk_size() -> Result {
         child
     };
 
-    Command::cargo_bin("chksum-cli")?.args(["-s", "512"]).arg(child.as_ref()).assert().success();
-    Command::cargo_bin("chksum-cli")?.args(["--chunk-size", "512"]).arg(child.as_ref()).assert().success();
-    Command::cargo_bin("chksum-cli")?.args(["--chunk-size", "1k"]).arg(child.as_ref()).assert().success();
-    Command::cargo_bin("chksum-cli")?.args(["--chunk-size", "1K"]).arg(child.as_ref()).assert().success();
-    Command::cargo_bin("chksum-cli")?.args(["--chunk-size", "1m"]).arg(child.as_ref()).assert().success();
-    Command::cargo_bin("chksum-cli")?.args(["--chunk-size", "1M"]).arg(child.as_ref()).assert().success();
-    Command::cargo_bin("chksum-cli")?.args(["--chunk-size", "1g"]).arg(child.as_ref()).assert().success();
-    Command::cargo_bin("chksum-cli")?.args(["--chunk-size", "1G"]).arg(child.as_ref()).assert().success();
-    Command::cargo_bin("chksum-cli")?.args(["--chunk-size", "-1"]).arg(child.as_ref()).assert().failure();
-    Command::cargo_bin("chksum-cli")?.args(["--chunk-size", "size"]).arg(child.as_ref()).assert().failure();
+    Command::cargo_bin("chksum-cli")?
+        .args(["-s", "512"])
+        .arg(child.as_ref())
+        .assert()
+        .success();
+    Command::cargo_bin("chksum-cli")?
+        .args(["--chunk-size", "512"])
+        .arg(child.as_ref())
+        .assert()
+        .success();
+    Command::cargo_bin("chksum-cli")?
+        .args(["--chunk-size", "1k"])
+        .arg(child.as_ref())
+        .assert()
+        .success();
+    Command::cargo_bin("chksum-cli")?
+        .args(["--chunk-size", "1K"])
+        .arg(child.as_ref())
+        .assert()
+        .success();
+    Command::cargo_bin("chksum-cli")?
+        .args(["--chunk-size", "1m"])
+        .arg(child.as_ref())
+        .assert()
+        .success();
+    Command::cargo_bin("chksum-cli")?
+        .args(["--chunk-size", "1M"])
+        .arg(child.as_ref())
+        .assert()
+        .success();
+    Command::cargo_bin("chksum-cli")?
+        .args(["--chunk-size", "1g"])
+        .arg(child.as_ref())
+        .assert()
+        .success();
+    Command::cargo_bin("chksum-cli")?
+        .args(["--chunk-size", "1G"])
+        .arg(child.as_ref())
+        .assert()
+        .success();
+    Command::cargo_bin("chksum-cli")?
+        .args(["--chunk-size", "-1"])
+        .arg(child.as_ref())
+        .assert()
+        .failure();
+    Command::cargo_bin("chksum-cli")?
+        .args(["--chunk-size", "size"])
+        .arg(child.as_ref())
+        .assert()
+        .failure();
 
     directory.close()?;
 
@@ -107,9 +146,21 @@ fn test_hash_md5() -> Result {
         child
     };
 
-    Command::cargo_bin("chksum-cli")?.args(["-H", "MD5"]).arg(child.as_ref()).assert().success();
-    Command::cargo_bin("chksum-cli")?.args(["--hash", "MD5"]).arg(child.as_ref()).assert().success();
-    Command::cargo_bin("chksum-cli")?.args(["--hash", "md5"]).arg(child.as_ref()).assert().failure();
+    Command::cargo_bin("chksum-cli")?
+        .args(["-H", "MD5"])
+        .arg(child.as_ref())
+        .assert()
+        .success();
+    Command::cargo_bin("chksum-cli")?
+        .args(["--hash", "MD5"])
+        .arg(child.as_ref())
+        .assert()
+        .success();
+    Command::cargo_bin("chksum-cli")?
+        .args(["--hash", "md5"])
+        .arg(child.as_ref())
+        .assert()
+        .failure();
 
     directory.close()?;
 
@@ -127,10 +178,26 @@ fn test_hash_sha1() -> Result {
         child
     };
 
-    Command::cargo_bin("chksum-cli")?.args(["-H", "SHA1"]).arg(child.as_ref()).assert().success();
-    Command::cargo_bin("chksum-cli")?.args(["--hash", "SHA1"]).arg(child.as_ref()).assert().success();
-    Command::cargo_bin("chksum-cli")?.args(["--hash", "SHA-1"]).arg(child.as_ref()).assert().success();
-    Command::cargo_bin("chksum-cli")?.args(["--hash", "sha1"]).arg(child.as_ref()).assert().failure();
+    Command::cargo_bin("chksum-cli")?
+        .args(["-H", "SHA1"])
+        .arg(child.as_ref())
+        .assert()
+        .success();
+    Command::cargo_bin("chksum-cli")?
+        .args(["--hash", "SHA1"])
+        .arg(child.as_ref())
+        .assert()
+        .success();
+    Command::cargo_bin("chksum-cli")?
+        .args(["--hash", "SHA-1"])
+        .arg(child.as_ref())
+        .assert()
+        .success();
+    Command::cargo_bin("chksum-cli")?
+        .args(["--hash", "sha1"])
+        .arg(child.as_ref())
+        .assert()
+        .failure();
 
     directory.close()?;
 

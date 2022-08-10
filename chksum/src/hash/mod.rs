f2fd614052d74e868cb0c6bb0494c190cae99b2e
Konrad Goławski <konrad@golawski.it>
2022-08-10 23:45:10 +0200

    Add tests for `HashDigest`

--- chksum/src/hash/mod.rs
+++ chksum/src/hash/mod.rs
@@ -80,3 +80,124 @@ impl UpperHex for HashDigest {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn hash_digest_from_md5() -> digest::Result<()> {
+        let digest = "D41D8CD98F00B204E9800998ECF8427E";
+        let digest = md5::Digest::try_from(digest)?;
+        assert_eq!(HashDigest::from(digest), HashDigest::MD5(digest));
+        Ok(())
+    }
+
+    #[test]
+    fn hash_digest_from_sha1() -> digest::Result<()> {
+        let digest = "DA39A3EE5E6B4B0D3255BFEF95601890AFD80709";
+        let digest = sha1::Digest::try_from(digest)?;
+        assert_eq!(HashDigest::from(digest), HashDigest::SHA1(digest));
+        Ok(())
+    }
+
+    #[test]
+    fn hash_digest_from_sha2_224() -> digest::Result<()> {
+        let digest = "D14A028C2A3A2BC9476102BB288234C415A2B01F828EA62AC5B3E42F";
+        let digest = sha2::sha224::Digest::try_from(digest)?;
+        assert_eq!(HashDigest::from(digest), HashDigest::SHA2_224(digest));
+        Ok(())
+    }
+
+    #[test]
+    fn hash_digest_from_sha2_256() -> digest::Result<()> {
+        let digest = "E3B0C44298FC1C149AFBF4C8996FB92427AE41E4649B934CA495991B7852B855";
+        let digest = sha2::sha256::Digest::try_from(digest)?;
+        assert_eq!(HashDigest::from(digest), HashDigest::SHA2_256(digest));
+        Ok(())
+    }
+
+    #[test]
+    fn hash_digest_md5_lower_hex() -> digest::Result<()> {
+        let digest = "D41D8CD98F00B204E9800998ECF8427E";
+        let digest = md5::Digest::try_from(digest)?;
+        let digest = HashDigest::MD5(digest);
+        assert_eq!(format!("{:x}", digest), "d41d8cd98f00b204e9800998ecf8427e");
+        Ok(())
+    }
+
+    #[test]
+    fn hash_digest_sha1_lower_hex() -> digest::Result<()> {
+        let digest = "DA39A3EE5E6B4B0D3255BFEF95601890AFD80709";
+        let digest = sha1::Digest::try_from(digest)?;
+        let digest = HashDigest::SHA1(digest);
+        assert_eq!(format!("{:x}", digest), "da39a3ee5e6b4b0d3255bfef95601890afd80709");
+        Ok(())
+    }
+
+    #[test]
+    fn hash_digest_sha2_224_lower_hex() -> digest::Result<()> {
+        let digest = "D14A028C2A3A2BC9476102BB288234C415A2B01F828EA62AC5B3E42F";
+        let digest = sha2::sha224::Digest::try_from(digest)?;
+        let digest = HashDigest::SHA2_224(digest);
+        assert_eq!(
+            format!("{:x}", digest),
+            "d14a028c2a3a2bc9476102bb288234c415a2b01f828ea62ac5b3e42f"
+        );
+        Ok(())
+    }
+
+    #[test]
+    fn hash_digest_sha2_256_lower_hex() -> digest::Result<()> {
+        let digest = "E3B0C44298FC1C149AFBF4C8996FB92427AE41E4649B934CA495991B7852B855";
+        let digest = sha2::sha256::Digest::try_from(digest)?;
+        let digest = HashDigest::SHA2_256(digest);
+        assert_eq!(
+            format!("{:x}", digest),
+            "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+        );
+        Ok(())
+    }
+
+    #[test]
+    fn hash_digest_md5_upper_hex() -> digest::Result<()> {
+        let digest = "d41d8cd98f00b204e9800998ecf8427e";
+        let digest = md5::Digest::try_from(digest)?;
+        let digest = HashDigest::MD5(digest);
+        assert_eq!(format!("{:X}", digest), "D41D8CD98F00B204E9800998ECF8427E");
+        Ok(())
+    }
+
+    #[test]
+    fn hash_digest_sha1_upper_hex() -> digest::Result<()> {
+        let digest = "da39a3ee5e6b4b0d3255bfef95601890afd80709";
+        let digest = sha1::Digest::try_from(digest)?;
+        let digest = HashDigest::SHA1(digest);
+        assert_eq!(format!("{:X}", digest), "DA39A3EE5E6B4B0D3255BFEF95601890AFD80709");
+        Ok(())
+    }
+
+    #[test]
+    fn hash_digest_sha2_224_upper_hex() -> digest::Result<()> {
+        let digest = "d14a028c2a3a2bc9476102bb288234c415a2b01f828ea62ac5b3e42f";
+        let digest = sha2::sha224::Digest::try_from(digest)?;
+        let digest = HashDigest::SHA2_224(digest);
+        assert_eq!(
+            format!("{:X}", digest),
+            "D14A028C2A3A2BC9476102BB288234C415A2B01F828EA62AC5B3E42F"
+        );
+        Ok(())
+    }
+
+    #[test]
+    fn hash_digest_sha2_256_upper_hex() -> digest::Result<()> {
+        let digest = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855";
+        let digest = sha2::sha256::Digest::try_from(digest)?;
+        let digest = HashDigest::SHA2_256(digest);
+        assert_eq!(
+            format!("{:X}", digest),
+            "E3B0C44298FC1C149AFBF4C8996FB92427AE41E4649B934CA495991B7852B855"
+        );
+        Ok(())
+    }
+}

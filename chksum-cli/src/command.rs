225db1722becde24a23d2592ed83a5190d2d6a29
Konrad Goławski <konrad@golawski.it>
2022-09-04 14:11:25 +0200

    Add missing hash algorithms after help

--- chksum-cli/src/command.rs
+++ chksum-cli/src/command.rs
@@ -15,5 +15,5 @@ pub(crate) fn create() -> Command<'static> {
                 .required(false)
                 .validator(|value| parse::human_number(value).map(|_| ())),
         ])
-        .after_help("Implemented hash algorithms:\n - MD5,\n - SHA-1.")
+        .after_help("Implemented hash algorithms:\n - MD5,\n - SHA-1,\n - SHA-2 224,\n - SHA-2 256,\n - SHA-2 512.")
 }
